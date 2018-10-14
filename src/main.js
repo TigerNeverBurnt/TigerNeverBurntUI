aa638911a5114eb1ac83b86f67e21bb41e511932
HuiMing Sun <bamboo1886@gmail.com>
2018-10-13 23:53:27 -0500

    add button

--- src/main.js
+++ src/main.js
@@ -6,10 +6,13 @@ import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import VueSpeech from 'vue-speech'
 import store from './store'
-
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(VueSpeech)
 Vue.use(ElementUI)
+Vue.use(Vuetify)
 Vue.use(Vuex)
 
 new Vue({
